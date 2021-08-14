13b0766c60c26fd887322af0f83bbb89e4e654c9
Kukjun Lee <20171655.info@edu.hanbat.ac.kr>
2021-08-14 12:30:29 +0900

    Upload Project
    이전에 작업했었던 Project를 리팩토링을 위해 commit 했습니다.

---
+++ stockSimulation/src/project_1/StockNameException.java
@@ -0,0 +1,8 @@
+package project_1;
+
+public class StockNameException extends Exception {
+    public StockNameException() {}
+    public StockNameException(String message) {
+        super(message);
+    }
+}
