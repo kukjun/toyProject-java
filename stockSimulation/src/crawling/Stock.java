1587ad2695ef1fd8e7911e6c9981a528a37dc0a0
Kukjun Lee <20171655.info@edu.hanbat.ac.kr>
2021-09-26 15:19:40 +0900

    Update Crawling part
    크롤링 스레드 분리, 동기화 작업 완료

--- stockSimulation/src/crawling/Stock.java
+++ stockSimulation/src/crawling/Stock.java
@@ -22,6 +22,16 @@ public class Stock {
     this.stockTodayLowLimitPrice = stockTodayLowLimitPrice;
   }
 
-
+  @Override
+  public String toString() {
+    String stockInfo;
+    stockInfo = "주식 이름 :" + stockName + "\n";
+    stockInfo += "주식의 가격 :" + stockPrice + "\n";
+    stockInfo += "오늘 최고가 :" + stockTodayHighPrice + "\n";
+    stockInfo += "오늘 최저가 :" + stockTodayLowPrice + "\n";
+    stockInfo += "오늘 상한가 :" + stockTodayHighLimitPrice + "\n";
+    stockInfo += "오늘 하한가 :" + stockTodayLowLimitPrice + "\n";
+    return stockInfo;
+  }
 
 }
